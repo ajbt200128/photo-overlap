df18a4aae92a76e136828c4c5bff01a1a0f9648d
ajbt200128 <austin@cutedogs.org>
2022-01-24 09:41:51 -0500

    Add: Code

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,42 @@
-use std::{time::Instant, collections::VecDeque};
+use std::{collections::VecDeque, time::Instant};
 
+use cached::proc_macro::cached;
 use image::{GenericImage, GenericImageView, Pixel, Rgb};
 use photon_rs::{
-    channels::{alter_blue_channel, alter_green_channel, alter_red_channel},
+    channels::{self, alter_blue_channel, alter_green_channel, alter_red_channel},
     conv::noise_reduction,
     helpers::dyn_image_from_raw,
     multiple::blend,
     native::{open_image, save_image},
-    transform::{crop, padding_bottom, padding_left, padding_right, padding_top, padding_uniform},
+    transform::{
+        crop, padding_bottom, padding_left, padding_right, padding_top, padding_uniform, resize,
+        SamplingFilter,
+    },
     PhotonImage, Rgba,
 };
+use clap::Parser;
+
+#[derive(Parser,Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args{
+    file:String,
+    learn:f64,
+    epsilon:f64,
+    scale:u32,
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut img = open_image("data/01007a.jpg").expect("File should open");
-    let orig = realign(img.clone(), Method::None,1000000.0,10.0);
-    let new = realign(img, Method::GradientDescent,1000000.0,10.0);
-    //let mut img = open_image("data/00056v.jpg").expect("File should open");
-    //let orig = realign(img.clone(), Method::None,1000.0,3.0);
-    //let new = realign(img, Method::GradientDescent,1000.0,3.0);
+    let args = Args::parse();
+    let img = open_image(&args.file).expect("File should open");
+    //let img = open_image("data/00056v.jpg").expect("File should open");
+    println!("w,h {} {}", img.get_width(), img.get_height());
+    let orig = realign(img.clone(), Method::None, 10000.0, 1.0, 1);
+    //let brute = realign(img.clone(), Method::Scale, 10000.0, 100.0, 1);
+    let new = realign(img, Method::GradientDescent, args.learn, args.epsilon, args.scale);
     save_image(orig, "img.jpg")?;
-    save_image(new, "img2.jpg")?;
+    save_image(new.clone(), "img2.jpg")?;
+    save_image(new, format!("processed/{}",args.file).as_str())?;
+    //save_image(brute, "img3.jpg")?;
     Ok(())
 }
 enum Method {
@@ -28,31 +45,110 @@ enum Method {
     None,
 }
 
-fn realign(mut img: PhotonImage, method: Method,delta:f64,epsilon:f64) -> PhotonImage {
+#[derive(Clone, Copy)]
+enum Channel {
+    R = 0,
+    G = 1,
+    B = 2,
+}
+#[derive(Clone, Copy)]
+struct Channels {
+    a: Channel,
+    b: Channel,
+}
+
+impl Channels {
+    fn new((a, b): (Channel, Channel)) -> Self {
+        Self { a, b }
+    }
+}
+impl From<(Channel, Channel)> for Channels {
+    fn from(p: (Channel, Channel)) -> Self {
+        Channels::new(p)
+    }
+}
+
+fn realign(img: PhotonImage, method: Method, delta: f64, epsilon: f64, scale: u32) -> PhotonImage {
+    let mut img_orig = img.clone();
+    let mut img = resize(
+        &img,
+        img.get_width() / scale,
+        img.get_height() / scale,
+        SamplingFilter::Nearest,
+    );
     let width = img.get_width();
     let height = img.get_height();
     let b_channel = crop(&mut img, 0, 0, width, height / 3);
     let g_channel = crop(&mut img, 0, height / 3, width, 2 * height / 3);
     let r_channel = crop(&mut img, 0, 2 * height / 3, width, 3 * height / 3);
-    let (mut r, mut g, mut b) = match method {
-        Method::Scale => todo!(),
-        Method::GradientDescent => {
-            let (r, g) = gradient_descent(r_channel, g_channel, delta, epsilon);
-            let (r, g) = gradient_descent(r, g, delta/2.0, epsilon/3.0);
-            let (g, b) = gradient_descent(g, b_channel, delta, epsilon);
-            let (g, b) = gradient_descent(g, b, delta/2.0, epsilon/3.0);
-            let (r, g) = gradient_descent(r, g, delta, epsilon);
-            let (r, g) = gradient_descent(r, g, delta/2.0, epsilon/3.0);
-            (r, g, b)
+    let (p_rg, p_gb,p_rb,p_rg_p, p_gb_p,p_rb_p) = match method {
+        Method::Scale => {
+            let (r, g, p_rg) =
+                brute_force(r_channel, g_channel, (Channel::R, Channel::G).into(), 15);
+            let (g, b, p_gb) = brute_force(g, b_channel, (Channel::G, Channel::B).into(), 15);
+            let (_, _, p_rb) = brute_force(r, b, (Channel::R, Channel::B).into(), 15);
+            (p_rg, p_gb,p_rb,0.0,0.0,0.0)
         }
-        Method::None => (r_channel, g_channel, b_channel),
+        Method::GradientDescent => {
+            let (g, b, p_gb) = gradient_descent(
+                g_channel,
+                b_channel,
+                (Channel::G, Channel::B).into(),
+                delta,
+                epsilon,
+                true,
+            );
+
+            let (g, b, p_gb_p) = gradient_descent(
+                g,
+                b,
+                (Channel::G, Channel::B).into(),
+                delta,
+                epsilon,
+                false,
+            );
+            let (r, g, p_rg) = gradient_descent(
+                r_channel,
+                g,
+                (Channel::R, Channel::G).into(),
+                delta,
+                epsilon,
+                true
+            );
+            let (r, g, p_rg_p) =
+                gradient_descent(r, g, (Channel::R, Channel::G).into(), delta, epsilon,false);
+
+            let (r, b, p_rb) =
+                gradient_descent(r, b, (Channel::R, Channel::B).into(), delta, epsilon,true);
+            let (r, b, p_rb_p) =
+                gradient_descent(r, b, (Channel::R, Channel::B).into(), delta, epsilon,false);
+            (p_rg, p_gb,p_rb,p_rg_p, p_gb_p,p_rb_p)
+        }
+        Method::None => (0.0, 0.0,0.0,0.0, 0.0,0.0),
     };
 
+    let width = img_orig.get_width();
+    let height = img_orig.get_height();
+    let p_rg = (p_rg * scale as f32) as i32;
+    let p_gb = (p_gb * scale as f32) as i32;
+    let p_rb = (p_rb * scale as f32) as i32;
+    let p_rg_p = (-p_rg_p * scale as f32) as i32;
+    let p_gb_p = (-p_gb_p * scale as f32) as i32;
+    let p_rb_p = (-p_rb_p * scale as f32) as i32;
+    let b = crop(&mut img_orig, 0, 0, width, height / 3);
+    let g = crop(&mut img_orig, 0, height / 3, width, 2 * height / 3);
+    let r = crop(&mut img_orig, 0, 2 * height / 3, width, 3 * height / 3);
+    let (mut g, mut b) = pad_photo(p_gb_p, &g, &b,false);
+    let (mut r, mut b) = pad_photo(p_rb, &r, &b,true);
+    let (r, g) = pad_photo(p_rg, &r, &g,true);
+    let (mut r, g) = pad_photo(p_rg_p, &r, &g,false);
+    let (mut g, b) = pad_photo(p_gb, &g, &b,true);
+    let (mut r, mut b) = pad_photo(p_rb_p, &r, &b,false);
+    println!("rg [{},{}] rb [{},{}] gb [{},{}]",p_rg,&p_rg_p,&p_rb,p_rb_p,p_gb,p_gb_p);
     reverse_grayscale(&mut r, 0);
     reverse_grayscale(&mut g, 1);
     reverse_grayscale(&mut b, 2);
     let orig = overlay(r, b);
-    save_image(orig.clone(), "inter.jpg").unwrap();
     overlay(orig, g)
 }
 
@@ -67,8 +163,6 @@ fn component_overlay(pi_a: &PhotonImage, pi_b: &PhotonImage) -> PhotonImage {
         .into_iter()
         .zip(pixels_b.into_iter())
         .for_each(|(mut a, b)| {
-            //////println!("{:?} {:?}", a.2.channels(),b.2.channels());
-
             a.2.channels_mut()[0] =
                 (a.2.channels_mut()[0] as u16 + b.2.channels()[0] as u16).clamp(0, 255) as u8;
             a.2.channels_mut()[1] =
@@ -82,10 +176,10 @@ fn component_overlay(pi_a: &PhotonImage, pi_b: &PhotonImage) -> PhotonImage {
 }
 
 fn overlay(mut pi_a: PhotonImage, mut pi_b: PhotonImage) -> PhotonImage {
-    let b_x_pad = pi_a.get_width() - pi_b.get_width();
     if pi_a.get_width() < pi_b.get_width() {
         std::mem::swap(&mut pi_a, &mut pi_b);
     }
+    let b_x_pad = pi_a.get_width() - pi_b.get_width();
     let mut pi_b = padding_right(&pi_b, b_x_pad, Rgba::new(0, 0, 0, 0));
 
     if pi_a.get_height() < pi_b.get_height() {
@@ -109,152 +203,143 @@ fn reverse_grayscale(photon_image: &mut PhotonImage, channel: usize) {
     }
 }
 
+// Should DP
 fn difference(pi_a: &PhotonImage, pi_b: &PhotonImage) -> f64 {
-    let start = Instant::now();
     let pixels_a = dyn_image_from_raw(pi_a);
     let pixels_a = pixels_a.pixels();
     let pixels_b = dyn_image_from_raw(pi_b);
     let pixels_b = pixels_b.pixels();
-    let end = Instant::now();
-    //let sum_sqd_diff:u128 = pixels_a.clone().into_iter().zip(pixels_b.clone().into_iter()).map(|((_,_,a),(_,_,b))| (a.channels()[0] as f64-b.channels()[0] as f64).powf(2.0) as u128).sum();
-    let start = Instant::now();
     let (a, b, c) = pixels_a
         .into_iter()
         .zip(pixels_b.into_iter())
         .map(|((_, _, a), (_, _, b))| {
+            let b = b.to_luma();
+            let a = a.to_luma();
+            let mut b = b.channels()[0] as f64;
+          //if a.channels()[3] == 0 {
+          //    b = 0.0;
+          //}
             let a = a.channels()[0] as f64;
-            let b = b.channels()[0] as f64;
             (a * b, a.powf(2.0), b.powf(2.0))
         })
         .fold((0.0, 0.0, 0.0), |(x, y, z), (a, b, c)| {
             (x + a, b + y, z + c)
         });
-    let ncc = a / (b.sqrt() * c.sqrt());
-    let end = Instant::now();
-    ////println!("time d: {:?}", end.duration_since(start));
-
-    //////println!("ssd {}",sum_sqd_diff);
-    ////println!("ncc {}", ncc);
-    return ncc;
+    a / (b.sqrt() * c.sqrt())
 }
 
-fn pad_photo(
-    (x, y): (f64, f64),
+fn pad_photo(x: i32, pi_a: &PhotonImage, pi_b: &PhotonImage,side:bool) -> (PhotonImage, PhotonImage) {
+    let (pi_a_y, pi_b_y) = if x > 0 { (x.abs(), 0) } else { (0, x.abs()) };
+    if side{
+        let pi_a_pad = padding_top(&pi_a, pi_a_y as u32, Rgba::new(255, 255, 255, 255));
+        let pi_b_pad = padding_top(&pi_b, pi_b_y as u32, Rgba::new(255, 255, 255, 255));
+        (pi_a_pad, pi_b_pad)
+    }else{
+        let pi_a_pad = padding_left(&pi_a, pi_a_y as u32, Rgba::new(255, 255, 255, 255));
+        let pi_b_pad = padding_left(&pi_b, pi_b_y as u32, Rgba::new(255, 255, 255, 255));
+        (pi_a_pad, pi_b_pad)
+    }
+}
+
+#[cached(
+    key = "(i32,u8,u8,bool)",
+    convert = r#"{(y,channels.a as u8,channels.b as u8,side)}"#
+)]
+fn pad_and_diff(
+    y: i32,
+    channels: Channels,
     pi_a: &PhotonImage,
     pi_b: &PhotonImage,
-) -> (PhotonImage, PhotonImage) {
-    let (pi_a_x, pi_b_x) = if x > 0.0 {
-        (x.round().abs(), 0.0)
-    } else {
-        (0.0, x.round().abs())
-    };
-    let (pi_a_y, pi_b_y) = if y > 0.0 {
-        (y.round().abs(), 0.0)
-    } else {
-        (0.0, y.round().abs())
-    };
-    let pi_a_pad = padding_left(&pi_a, pi_a_x as u32, Rgba::new(0, 0, 0, 0));
-    let pi_a_pad = padding_top(&pi_a, pi_a_y as u32, Rgba::new(0, 0, 0, 0));
-    let pi_b_pad = padding_left(&pi_b, pi_b_x as u32, Rgba::new(0, 0, 0, 0));
-    let pi_b_pad = padding_top(&pi_b, pi_b_y as u32, Rgba::new(0, 0, 0, 0));
-    (pi_a_pad, pi_b_pad)
+    side:bool,
+) -> (PhotonImage, PhotonImage, f64) {
+    let (pi_a_pad, pi_b_pad) = pad_photo(y, pi_a, pi_b,true);
+    let diff = difference(&pi_a_pad, &pi_b_pad);
+    (pi_a_pad, pi_b_pad, diff)
+}
+
+fn brute_force(
+    pi_a: PhotonImage,
+    pi_b: PhotonImage,
+    channels: Channels,
+    search_radius: i32,
+) -> (PhotonImage, PhotonImage, f32) {
+    let mut best = 0.0;
+    let mut best_a = pi_a.clone();
+    let mut best_b = pi_b.clone();
+    let mut best_p = 0.0;
+    for y in (-search_radius)..search_radius {
+        println!("{}", y);
+        let (pi_a_pad, pi_b_pad, diff) = pad_and_diff(y, channels, &pi_a, &pi_b,true);
+        if diff > best {
+            println!("{} {}", best, diff);
+            best = diff;
+            best_a = pi_a_pad;
+            best_b = pi_b_pad;
+            best_p = y as f32;
+        }
+    }
+    (best_a, best_b, best_p)
 }
 
 fn gradient_descent(
     pi_a: PhotonImage,
     pi_b: PhotonImage,
+    channels: Channels,
     delta: f64,
-    epsilon: f64,
-) -> (PhotonImage, PhotonImage) {
-    let mut padding_x = 0.0;
-    let mut padding_y = 0.0;
-    let mut seen_x:VecDeque<i32> = VecDeque::new();
-    let mut seen_y:VecDeque<i32> = VecDeque::new();
-    let (mut seen_x_cnt,mut seen_y_cnt) = (0,0);
-    let mut delta_x = delta;
-    let mut delta_y = delta;
-    let mut epsilon_x = epsilon;
-    let mut epsilon_y = epsilon;
-    let mut rng = rand::thread_rng();
+    mut epsilon: f64,
+    side:bool
+) -> (PhotonImage, PhotonImage, f32) {
+    let mut padding = 0.0;
+    let mut seen: VecDeque<i32> = VecDeque::new();
+    let mut seen_cnt = 0;
+    let mut delta = delta;
+    let mut best = 1.0;
+    let mut best_pad = 0.0;
     let mut pi_a_pad_final = pi_a.clone();
     let mut pi_b_pad_final = pi_b.clone();
     for _ in 1..300 {
-        let start = Instant::now();
-        let (pi_a_pad, pi_b_pad) = pad_photo((padding_x, padding_y), &pi_a, &pi_b);
-        let (pi_a_pad_x, pi_b_pad_x) = pad_photo((padding_x + epsilon_x, padding_y), &pi_a, &pi_b);
-        let (pi_a_pad_y, pi_b_pad_y) = pad_photo((padding_x, padding_y + epsilon_y), &pi_a, &pi_b);
-        let end = Instant::now();
+        let (pi_a_pad, pi_b_pad, diff) = pad_and_diff((padding) as i32, channels, &pi_a, &pi_b,side);
+        let (_, _, diff_p) = pad_and_diff((padding + epsilon) as i32, channels, &pi_a, &pi_b,side);
 
-        let start = Instant::now();
-        let diff = 1.0 - difference(&pi_a_pad, &pi_b_pad);
-        let diff_x_p = 1.0 - difference(&pi_a_pad_x, &pi_b_pad_x);
-        let diff_y_p = 1.0 - difference(&pi_a_pad_y, &pi_b_pad_y);
-        let end = Instant::now();
-        let mut gradient_x =  delta_x * ((diff - diff_x_p) / epsilon_x);
-        let mut gradient_y = delta_y * (diff - diff_y_p) / epsilon_y;
-        println!("G_Y {}",gradient_y);
-      //if gradient_x.abs() != 0.0{
-      //    padding_x -= gradient_x.abs().log2() * sign_x;
-      //}
-      //if gradient_y.abs() != 0.0{
-      //    padding_y -= gradient_y.abs().log2() * sign_y;
-      //}
-        let sign_x = gradient_x / gradient_x.abs();
-        let sign_y = gradient_y / gradient_y.abs();
+        let diff = 1.0 - diff;
+        let diff_p = 1.0 - diff_p;
+        let mut gradient = delta * ((diff - diff_p) / epsilon);
+        let sign = gradient / gradient.abs();
 
-        if gradient_x.abs() >= 1.0{
-            gradient_x = gradient_x.abs().log2() * sign_x
+        if gradient.abs() >= 1.0 {
+            gradient = gradient.abs().log2() * sign;
         }
-        if gradient_y.abs() >= 1.0{
-            gradient_y = gradient_y.abs().log2() * sign_y;
+        gradient = (gradient * 100.0).trunc() / 100.0;
+        if seen.contains(&(gradient as i32)) {
+            seen_cnt += 1;
+        } else {
+            seen_cnt = 0;
         }
-        gradient_x = (gradient_x * 100.0).trunc() / 100.0;
-        gradient_y = (gradient_y * 100.0).trunc() / 100.0;
-        if seen_x.contains(&(gradient_x as i32)){
-            seen_x_cnt+=1;
-        }else{
-            seen_x_cnt = 0;
+        seen.push_back(gradient as i32);
+        if seen.len() > 10 {
+            seen.pop_front();
         }
-        if seen_y.contains(&(gradient_y as i32)){
-            seen_y_cnt+=1;
-        }else{
-            seen_y_cnt = 0;
+        println!(
+            "x: {} {:.5} {:.5} {:.5} {} {:?}",
+            gradient, padding, diff, diff_p, delta, epsilon
+        );
+        padding += gradient;
+        if seen_cnt > 4 {
+            delta /= 10.0;
         }
-        seen_x.push_back(gradient_x as i32);
-        seen_y.push_back(gradient_y as i32);
-        if seen_x.len() > 10{
-            seen_x.pop_front();
+        if epsilon > gradient {
+            epsilon/=1.1;
         }
-        if seen_y.len() > 10{
-            seen_y.pop_front();
+        if diff < best {
+            best = diff;
+            pi_a_pad_final = pi_a_pad;
+            pi_b_pad_final = pi_b_pad;
+            best_pad = padding
         }
-        padding_x += gradient_x;
-        padding_y += gradient_y;
-        println!("x: {} {:.5} {:.5} {:.5} {} {:?}", gradient_x, padding_x, diff, diff_x_p,delta_x,epsilon_x);
-        println!("y: {} {:.5} {:.5} {:.5} {} {:?}", gradient_y, padding_y, diff, diff_y_p,delta_y,epsilon_y);
-      //if gradient_x.abs() < epsilon_x && epsilon_x >= 1.1 {
-      //    epsilon_x /= 1.1;
-      //}
-      //if gradient_y.abs() < epsilon_y && epsilon_y >= 1.1 {
-      //    epsilon_y /= 1.1;
-      //}
-        if seen_x_cnt > 4{
-            delta_x /= 10.0;
-           // println!("A--------------");
-            //return (pi_a_pad, pi_b_pad);
+        if gradient.abs() < 0.01 {
+            println!("-------");
+            return (pi_a_pad_final, pi_b_pad_final, best_pad as f32);
         }
-        if seen_y_cnt > 4{
-            delta_y /= 10.0;
-            // println!("A--------------");
-            //return (pi_a_pad, pi_b_pad);
-        }
-        if gradient_x.abs() < 0.01 && gradient_y.abs() < 0.01{
-            println!("B--------------");
-            return (pi_a_pad, pi_b_pad);
-        }
-        pi_a_pad_final = pi_a_pad;
-        pi_b_pad_final = pi_b_pad;
     }
-    println!("C--------------");
-    (pi_a_pad_final, pi_b_pad_final)
+    (pi_a_pad_final, pi_b_pad_final, best_pad as f32)
 }
